77a5c32acc35339810bfd1148dbb6a090c627bda
Florian Touya (deuzu) <florian.touya@pm.me>
2021-04-01 21:58:18 +0200

    chore: 1st commit

---
+++ src/main.ts
@@ -0,0 +1,106 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+async function run(): Promise<void> {
+  try {
+      core.debug(`Implementing democracy on org/repo. Resistence is futile.`)
+
+      const token = core.getInput('github-token')
+      const octokit = github.getOctokit(token)
+
+      const context = github.context;
+      core.debug(token)
+      core.debug(JSON.stringify(context))
+      // const { data: pullRequest } = await octokit.pulls.list({
+      //   owner: 'octokit',
+      //   repo: 'rest.js',
+      //   pull_number: 123,
+      //   mediaType: {
+      //     format: 'diff'
+      //   }
+      // });
+
+      core.debug('Democracy will be back.')
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+const listPullRequests = async () => {
+  const requestBody = await requestGithub({ url: pullRequestUrl + '?state=open' })
+
+  return JSON.parse(requestBody)
+}
+
+const getPullRequestsVotes = async pullRequests => {
+  const votes = {}
+
+  for (const pullRequest of pullRequests) {
+    // const data = await Promise.all(getPullRequest(pullRequest.number), getIssue(pullRequest.number), getLastCommit(pullRequest.number))
+    const singlePullRequest = await getPullRequest(pullRequest.number)
+    const issue = await getIssue(pullRequest.number)
+    const lastCommit = await getLastCommit(pullRequest.number)
+
+    const pullRequestData = {
+      updatedAt: lastCommit.commit.committer.date,
+      title: pullRequest.title,
+      mergeable: singlePullRequest.mergeable,
+    }
+
+    if (!validatePullRequest(pullRequestData)) {
+      continue
+    }
+
+    votes[pullRequest.number] = await getVoteResult(pullRequest.number)
+  }
+
+  return votes
+}
+
+
+const getVoteResult = async pullRequestNumber => {
+  const reactions = await listPullRequestReactions(pullRequestNumber)
+  const voters = await getOrganizationMembers()
+
+  let voteResult = 0
+  const voteReactionRegex = new RegExp('(\\+|-)1')
+
+  reactions
+    .filter(reaction => {
+      const reactionIsVote = voteReactionRegex.test(reaction.content)
+      const userHasRightToVote = voters.indexOf(reaction.user.id)
+
+      return reactionIsVote && userHasRightToVote
+    })
+    .map(reaction => voteResult = voteResult + parseInt(reaction.content))
+
+
+  return voteResult
+}
+
+const validatePullRequest = (pullRequest) => {
+  const now = moment().utc()
+  const updatedAt24hoursForward = moment(pullRequest.updatedAt).utc().add(24, 'h')
+  const pullRequestIsMature = updatedAt24hoursForward.diff(now, 'minutes') < 0
+  const pullRequestIsReadyToMerge = pullRequest.title.trim().startsWith(pullRequestReadyToMergePrefix)
+  const pullRequestIsMergeable = pullRequest.mergeable
+
+  return pullRequestIsMature && pullRequestIsReadyToMerge && pullRequestIsMergeable
+}
+
+const processPullRequest = async pullRequestsVoteResults => {
+  const voteResult = {}
+
+  for (const i in pullRequestsVoteResults) {
+    const pullRequestVoteResult = pullRequestsVoteResults[i]
+    if (pullRequestVoteResult >= 2) {
+      log(`Democracy has spoken. Pull Request #${i} has been voted for merge.`)
+      await mergePullRequest(i)
+      log(`Pull Request #${i} merged.`)
+    } else {
+      log(`Democracy has spoken. Pull Request #${i} has not been been voted to merge. Ignored.`)
+    }
+  }
+}
+
+run()
